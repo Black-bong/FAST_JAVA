3beeadd02562e999c289150282e05abd370daf96
Black_Bong <shoy1415@naver.com>
2021-07-21 17:52:34 +0900

    get/set 실습

---
+++ Chapter5/src/hiding/MyDateTest.java
@@ -0,0 +1,13 @@
+package hiding;
+
+public class MyDateTest {
+    public static void main(String[] args) {
+        MyDate myDate = new MyDate();
+
+        myDate.setDay(21);
+        myDate.setMonth(70);
+        myDate.setYear(2021);
+
+        myDate.showDate();
+    }
+}
