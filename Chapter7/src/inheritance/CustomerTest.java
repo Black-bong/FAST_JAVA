8d90d1d15b6096e88818178f1e90af531f978814
Black_Bong <shoy1415@naver.com>
2021-07-23 17:58:16 +0900

    상속에서 클래스 생성 과정 및 형변환 실습

---
+++ Chapter7/src/inheritance/CustomerTest.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+//        Customer customerLee = new Customer();
+//        customerLee.setCustomerName("이순신");
+//        customerLee.setCustomerID(10010);
+//        customerLee.bonusPoint = 1000;
+
+        VIPCustomer customerKim = new VIPCustomer("김유신", 10020);
+//        customerKim.setCustomerName("김유신");
+//        customerKim.setCustomerID(10020);
+        customerKim.bonusPoint = 10000;
+
+//        System.out.println(customerLee.showCustomerInfo());
+        System.out.println(customerKim.showCustomerInfo());
+    }
+}
