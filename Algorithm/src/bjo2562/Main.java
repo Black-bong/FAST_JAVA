90a5c1311985e22d19f3fbf25a7a85ca4ff97e41
Black_Bong <shoy1415@naver.com>
2021-07-29 15:50:53 +0900

    bjo2562

--- Algorithm/src/bjo2562/Main.java
+++ Algorithm/src/bjo2562/Main.java
@@ -5,17 +5,33 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        ArrayList<Integer> numberList = new ArrayList<Integer>();
+//
+//        for(int i=0; i<9; i++) {
+//            int inputNumber = scan.nextInt();
+//            numberList.add(inputNumber);
+//        }
+//        int max = Collections.max(numberList);
+//        System.out.println(max);
+//        int index = numberList.indexOf(max);
+//        System.out.println(index+1);
+//    }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> numberList = new ArrayList<Integer>();
-
-        for(int i=0; i<9; i++) {
-            int inputNumber = scan.nextInt();
-            numberList.add(inputNumber);
+        Scanner scanner = new Scanner(System.in);
+        int[] numberList = new int[9];
+        int max = numberList[0];
+        int index = 0;
+        for(int i=0;i<numberList.length;i++) {
+            int inputNumber = scanner.nextInt();
+            numberList[i] = inputNumber;
+            if(max < numberList[i]) {
+                max = numberList[i];
+                index = i;
+            }
         }
-        int max = Collections.max(numberList);
         System.out.println(max);
-        int index = numberList.indexOf(max);
         System.out.println(index+1);
     }
 }
