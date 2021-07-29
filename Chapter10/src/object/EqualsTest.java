e9dd5568040dff6fbc482d5929f4ba18bbf8651f
Black_Bong <shoy1415@naver.com>
2021-07-29 13:00:02 +0900

    equals 메소드 활용

---
+++ Chapter10/src/object/EqualsTest.java
@@ -0,0 +1,42 @@
+package object;
+
+class Student{
+    int studentNum;
+    String studentName;
+
+    public Student(int studentNum, String studentName) {
+        this.studentNum = studentNum;
+        this.studentName = studentName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Student) {
+            Student std = (Student)obj;
+            return (this.studentNum == std.studentNum);
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+public class EqualsTest {
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+
+        System.out.println("==============");
+
+        Student studentLee = new Student(100, "Lee");
+        Student studentLee2 = studentLee;
+        Student studentSin = new Student(100, "Lee");
+
+        System.out.println(studentLee == studentLee2);
+        System.out.println(studentLee == studentSin);
+        System.out.println(studentLee.equals(studentSin));
+    }
+}
