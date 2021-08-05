b7d6da81c3816f4292491f5ecee8f79147cd6e74
Black_Bong <shoy1415@naver.com>
2021-08-05 09:43:20 +0900

    file out/input stream 예제

--- Chapter14/src/stream/outputstream/FileOutputTest.java
+++ Chapter14/src/stream/outputstream/FileOutputTest.java
@@ -1,15 +1,26 @@
 package stream.outputstream;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileOutputTest {
     public static void main(String[] args) {
-        try (FileOutputStream fis = new FileOutputStream("Chapter14/src/stream/outputstream/output.txt", true)) {
-            fis.write(65);
-            fis.write(66);
-            fis.write(67);
-            fis.write(68);
+
+        byte[] by = new byte[26];
+        byte data = 65;
+        for (int i=0;i<by.length;i++) {
+            by[i] = data;
+            data++;
+        }
+        try (FileOutputStream fos = new FileOutputStream("Chapter14/src/stream/outputstream/alpha.txt", true);
+             FileInputStream fis = new FileInputStream("Chapter14/src/stream/outputstream/alpha.txt")){
+
+            fos.write(by);
+            int ch;
+            while ((ch = fis.read()) != -1) {
+                System.out.print((char)ch);
+            }
         } catch (IOException e) {
             System.out.println(e);
         }
